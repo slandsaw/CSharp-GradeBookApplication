4377a626bd44c6f07b5fab58674829f676b6776a
Steve Landsaw <slandsaw@resurgent.com>
2018-11-30 17:08:27 -0500

    Create new GradeBookTypes

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,12 +13,15 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {
             Name = name;
             Students = new List<Student>();
         }
+
+        
 
         public void AddStudent(Student student)
         {
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        // This is a constructor
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
